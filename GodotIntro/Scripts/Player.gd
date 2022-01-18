8767b068e89949819fd127165366123e180bcff4
Nabiz <dawcio.gwizdz@gmail.com>
2022-01-18 22:16:02 +0100

    player animation change

--- GodotIntro/Scripts/Player.gd
+++ GodotIntro/Scripts/Player.gd
@@ -6,7 +6,9 @@ export var jump_speed = -200
 
 var velocity = Vector2.ZERO
 
-func _process(delta):
+onready var animation_sprite = $AnimatedSprite
+
+func _process(_delta):
     if Input.is_action_pressed("ui_right"):
         velocity.x = speed
     elif Input.is_action_pressed("ui_left"):
@@ -17,6 +19,19 @@ func _process(delta):
     if Input.is_action_pressed("ui_up") and is_on_floor():
         velocity.y = jump_speed
 
+    set_animation()
+
 func _physics_process(delta):
     velocity.y += gravity*delta
     velocity = move_and_slide(velocity, Vector2.UP)
+
+func set_animation():
+    if velocity == Vector2.ZERO:
+        animation_sprite.animation = "idle"
+    elif velocity.y == 0 and velocity.x != 0:
+        animation_sprite.animation = "run"
+    
+    if velocity.x < 0:
+        animation_sprite.flip_h = true
+    else:
+        animation_sprite.flip_h = false
